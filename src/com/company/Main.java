b2077b4042613da0d44769870c4242a6253bbbe0
Александра <aleksandra@MacBook-Pro-Andrey.local>
2022-06-09 14:10:28 +0300

    add new files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
-        myDog.run();
-        myDog.makeSound();
-        myCat.run();
-        myCat.makeSound();
+        Math math = new Math();
+        int result1 = math.calc(10, 5);
+        float result2 = math.calc(8, 12.5F);
+        double result3 = math.calc(6, 7.3);
+        System.out.println("int: " + result1);
+        System.out.println("float: " + result2);
+        System.out.println("double: " + result3);
+
     }
 }
---
+++ src/com/company/Math.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Math {
+    int calc(int a, int b){
+        return a + b;
+    }
+
+    float calc (int a, float b) {
+        return a + b;
+    }
+
+    double calc (int a, double b){
+        return a + b;
+    }
+}
