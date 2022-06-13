f59b5244c8695a07d8465f373f7fe30a8d418da7
Александра <aleksandra@MacBook-Pro-Andrey.local>
2022-06-13 16:33:16 +0300

    add new files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,27 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Main {
     public static void main(String[] args) {
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
-        myDog.run();
-        myDog.makeSound();
-        myCat.run();
-        myCat.makeSound();
+        ArrayList<String> strList = new ArrayList<String>();
+        strList.add("first string");
+        strList.add("second");
+        strList.add("third");
+
+        System.out.println(findString(strList, "second"));
+        System.out.println(findString(strList, "fourth"));
+    }
+
+    public static boolean findString(ArrayList<String> strList, String str) {
+        AtomicBoolean isSubstringFound = new AtomicBoolean(false);
+
+        strList.forEach((strItem) -> {
+            if (strItem == str) {
+                isSubstringFound.set(true);
+            }
+        });
+
+        return isSubstringFound.get();
     }
 }
