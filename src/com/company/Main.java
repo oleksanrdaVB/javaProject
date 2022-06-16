2a071e350a4371f15fa94ee818b94c24a0d5aa42
Александра <aleksandra@MacBook-Pro-Andrey.local>
2022-06-16 15:53:08 +0300

    add new task

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,22 @@
 package com.company;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Animal myDog = new Dog();
-        Animal myCat = new Cat();
-        myDog.run();
-        myDog.makeSound();
-        myCat.run();
-        myCat.makeSound();
+        HashMap <String, String> Customers = new HashMap<String, String>();
+        Customers.put("1", "Erik Horlings");
+        Customers.put("2", "Wilfred van der Plaats");
+        Customers.put("3", "Wiepko Bos");
+        Customers.put("4", "Dewi Bakker");
+        Customers.put("5", "Gokhan Poyraz");
+
+        System.out.println("Name at value 2: "+ Customers.get("2"));
+
+        String removedValue = Customers.remove("4");
+        System.out.println("Removed value: " + removedValue);
+
+        System.out.println("Updated HashMap: " + Customers);
+
     }
+
 }
